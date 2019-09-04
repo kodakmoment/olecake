31dc12059ced8fa7c9d15c3ad8a86a07b058ecf6
olejobbpc <ole.hustad@gmail.com>
2019-09-04 09:43:33 +0200

    Added ejs and index page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.json())
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
 app.get('/', function (req,res) {
-  res.end('<!DOCTYPE html><html lang="en"><body><center><br><br><br><img width="640" height="400" src="olecake.jpg"><br><br><h2>The ole cake</h2></center></body></html>')
+  res.render('pages/index');
 });
 
 app.listen(8080, function() {console.log('Server running on port 8080')});
