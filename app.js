787811e7f8fdaf8ebbec1c5115ee6f08f17b8658
Ole Severin Hustad <ole.severin.hustad@basefarm.com>
2022-08-18 09:54:21 +0200

    update olecake

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const app = express();
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
+let express = require('express');
+let app = express();
+let morgan = require('morgan');
+let port = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
-app.get('/', function (req,res) {
+app.get('/', function (req, res) {
   res.render('pages/index');
 });
 
-app.listen(8080, function() {console.log('Server running on port 8080')});
+app.listen(process.env.PORT || port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
